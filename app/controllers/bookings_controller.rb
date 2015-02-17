e183c23aa6bce15af251391da470b93166a08cd3
LilianBich <lilian.bich@gmail.com>
2015-02-17 17:30:56 +0100

    tipo doc productsss, root update to nest booking, booking controler done (new&create) with view (new)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,25 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
 
   def new
-
+    @product = Product.find(params[:product_id])
+    @booking = Booking.new
   end
 
   def create
+    customer = current_user
+    @product = Product.find(params[:product_id])
+    @booking = @product.bookings.build(booking_params)
+    @booking.customer = customer
+    if @booking.save
+      redirect_to account_da  root_path
+    else
+      render :new
+    end
+  end
+
+  def booking_params
+      params.require(:booking).permit(:check_in, :message, :night)
   end
 end
+
