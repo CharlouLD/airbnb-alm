1daf12785e918f86105540ea863ea1dbe5cf9462
LilianBich <lilian.bich@gmail.com>
2015-02-17 18:50:33 +0100

    Commented picture issue

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     # Random generation of flat, partial to code
-    # @products = Product.all.sample(3)
+    @products = Product.all.sample(3)
   end
 end
