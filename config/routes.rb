6dcf69284886e9ff9d0748d6790e75202ec24475
maximedelpit <maximedelpit@gmail.com>
2015-02-18 11:26:30 +0100

    remove useless get on incoming_bookings index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'incoming_bookings/index'
-
   # devise_for :users
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "home#index"
