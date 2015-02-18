6d1e9afe0a13f0f620654628ba0310701305e5ee
LilianBich <lilian.bich@gmail.com>
2015-02-18 15:20:34 +0100

    work done

--- app/controllers/account/products_controller.rb
+++ app/controllers/account/products_controller.rb
@@ -1,6 +1,5 @@
 module Account
   class ProductsController < ApplicationController
-    layout :account
 
     def index
       user = current_user
@@ -8,18 +7,20 @@ module Account
     end
 
     def edit
-      @product = Product.find(params[:product_id])
+      @product = Product.find(params[:id])
     end
 
     def update
-      owner = current_user
-      @product = Product.new(product_params)
-      @product.owner = owner
-      if @product.save
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
            redirect_to product_path(@product)
          else
            render :new
          end
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :description, :picture, :online, :city, :night_price)
     end
   end
 end
