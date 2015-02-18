8cd7448d828865856a2f4038499190d3cbff13f6
LilianBich <lilian.bich@gmail.com>
2015-02-18 17:06:36 +0100

    add picture to navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,7 +19,25 @@ class BookingsController < ApplicationController
   end
 
   def index
-    @booking = Booking.all
+    user = current_user
+    @bookings = user.bookings
+  end
+
+  def edit
+    @product = Product.find(params[:product_id])
+    @booking = Booking.new
+  end
+
+  def create
+    customer = current_user
+    @product = Product.find(params[:product_id])
+    @booking = @product.bookings.build(booking_params)
+    @booking.customer = customer
+    if @booking.save
+      redirect_to account_dashboards_path
+    else
+      render :new
+    end
   end
 
 
