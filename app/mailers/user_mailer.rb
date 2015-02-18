4d20aaebf5f13b4be1f9b9c0bc83f203bdd80b1d
LilianBich <lilian.bich@gmail.com>
2015-02-18 21:02:05 +0100

    adding unsaved changes + mailer enhenced

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'your-email@example.com'
+  default from: 'team@airbnb-alm.com'
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
