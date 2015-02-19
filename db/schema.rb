ba808f8c1294cb5fb546427b3022c9fc4113d19d
LilianBich <lilian.bich@gmail.com>
2015-02-19 16:26:05 +0100

    migrating city to address

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219104103) do
+ActiveRecord::Schema.define(version: 20150219110417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150219104103) do
     t.string   "title"
     t.text     "description"
     t.integer  "user_id"
-    t.string   "city"
+    t.string   "address"
     t.integer  "night_price"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
