f4787525dc0d034eafe7a1a25145348e5fe34ca0
LilianBich <lilian.bich@gmail.com>
2015-02-20 16:02:37 +0100

    adding url of heroku for production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   ## TODO add the url when deploy on heroku
-  # config.action_mailer.default_url_options = { host: 'localhost'}
+  config.action_mailer.default_url_options = { host: 'https://airbnb-alm.herokuapp.com'}
 
 end
